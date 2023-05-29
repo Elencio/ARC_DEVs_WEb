65fd8fe37fa6b6f9edc5f4cca26d7b19e9da4360
elencio <elenciocalado@gmail.com>
2023-05-28 22:41:55 -0300

    atualizando o gap do grid

--- src/Web/OurFeatures.tsx
+++ src/Web/OurFeatures.tsx
@@ -34,7 +34,7 @@ const OurFeaturesSectionWrapper = styled.div`
 const FeaturesWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(200px, 2fr));
-  grid-gap: 2rem;
+  grid-gap: 3rem;
   gap: 1rem;
   align-items: center;
   flex-wrap: wrap;
